aaa6666b68a202b64d56f2494eb2f911588d8ee8
KKasyanova <eliseeva.kseniya@gmail.com>
2022-10-09 15:19:45 +0500

    Задача №9_Циклы

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,41 @@
+/*
+Напишите программу:
+
+
+        1. Пользователь вводит размер массива и данные с клавиатуры в массив типа double.
+
+        2. Посчитайте среднее арифметическое элементов массива.
+
+        3. После этого произведите вывод массива на экран, где каждый элемент массива умножается на среднее арифметическое
+*/
+
+package com.max.idea;
+
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Введите размер массива: ");
+        int arrayL = in.nextInt(); //длина массива
+
+        double[] myArray = new double[arrayL]; //задать длину массива
+        for (int i=0;i<myArray.length;i++){
+            System.out.println("Введите значение элемента "+i+" : ");
+            myArray[i] = in.nextDouble();//присвоить значение каждому следующему элементу массива, начиная с нулевого
+        }
+
+        //посчитать среднее арифметическое массива
+        double sum = 0;
+        for (int i=0;i<myArray.length;i++){
+            sum = sum + myArray[i];
+        }
+        double avg = sum/myArray.length;
+
+        //вывести все элементы массива, умноженные на среднее арифметическое
+        for (int i=0;i<myArray.length;i++){
+            myArray[i] = myArray[i]*avg;
+            System.out.println(myArray[i]);
+        }
     }
 }
